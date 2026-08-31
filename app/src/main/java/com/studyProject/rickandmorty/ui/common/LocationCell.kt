6e824282b107b1e18c024704c398f28a0456c213
Clara Muniz <179251307+claraMuniz0@users.noreply.github.com>
2026-08-31 12:09:49 -0300

    created location cell

---
+++ app/src/main/java/com/studyProject/rickandmorty/ui/common/LocationCell.kt
@@ -0,0 +1,107 @@
+package com.studyProject.rickandmorty.ui.common
+
+import androidx.compose.foundation.background
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontWeight
+import com.studyProject.rickandmorty.ui.theme.RickAndMortyTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
+import com.studyProject.rickandmorty.ui.theme.RMGreen
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material.icons.filled.Public
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.Alignment
+import com.studyProject.rickandmorty.ui.theme.RMBrown
+import com.studyProject.rickandmorty.ui.theme.RMPink
+import com.studyProject.rickandmorty.ui.theme.RMYellow
+
+@Composable
+fun LocationCell(
+    id: Int,
+    name: String,
+    type: String,
+    dimension: String
+) {
+    Box(
+        modifier = Modifier
+            .size(160.dp, 98.dp)
+            .background(
+                color = Color.LightGray,
+                shape = RoundedCornerShape(10.dp)
+            )
+    ) {
+
+        Column(
+            modifier = Modifier.padding(horizontal = 15.dp, vertical = 15.dp),
+        ) {
+            LocationInfo(id, name, type, dimension)
+        }
+    }
+}
+
+@Composable
+private fun LocationInfo(
+    id: Int,
+    name: String,
+    type: String,
+    dimension: String
+) {
+    Column() {
+        val colorArray: Array<Color> = arrayOf(RMPink, RMGreen, RMYellow)
+
+        Icon(
+            imageVector = locationTypeIcon(type),
+            contentDescription = type,
+            tint = colorArray[(id - 1) % colorArray.size],
+            modifier = Modifier.size(22.dp)
+        )
+
+        Text(
+            text = name,
+            color = RMBrown,
+            fontSize = 15.sp,
+            fontWeight = FontWeight.Bold,
+            maxLines = 1,
+            modifier = Modifier
+                .padding(top = 5.dp)
+        )
+
+        Text(
+            text = dimension,
+            color = Color.DarkGray,
+            fontSize = 11.sp,
+            fontWeight = FontWeight.Medium,
+        )
+    }
+}
+
+private fun locationTypeIcon(type: String): ImageVector {
+    return when (type) {
+        "Planet" -> Icons.Filled.Public
+        else -> Icons.Filled.Place
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+private fun LocationCellPreview() {
+    RickAndMortyTheme {
+        LocationCell(
+            1,
+            "Earth",
+            "Planet",
+            "Dimension C-137"
+        )
+    }
+}
